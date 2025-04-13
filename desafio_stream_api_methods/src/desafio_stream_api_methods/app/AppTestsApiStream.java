55d8e69b699571147d9e6191cc595ebc77c07538
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-04-13 19:45:02 -0400

    feat: desafio 2 - soma dos números pares com Stream API

--- desafio_stream_api_methods/src/desafio_stream_api_methods/app/AppTestsApiStream.java
+++ desafio_stream_api_methods/src/desafio_stream_api_methods/app/AppTestsApiStream.java
@@ -9,9 +9,13 @@ public class AppTestsApiStream {
 
 //		 Desafio 1 - Mostre a lista na ordem numérica:
 
-//		 Crie um programa que utilize a Stream API para ordenar a lista de números em
-//		 ordem crescente e a exiba no console.
+//		 Crie um programa que utilize a Stream API para ordenar a lista de números em ordem crescente e a exiba no console.
+//		numeros.stream().sorted().forEach(System.out::println);
 
-		numeros.stream().sorted().forEach(System.out::println);
+//		Desafio 2 - Imprima a soma dos números pares da lista:
+
+//		Utilizando a Stream API, realize a soma dos números pares da lista e exiba o resultado no console.
+		int somaPares = numeros.stream().filter(n -> n % 2 == 0).mapToInt(Integer::intValue).sum();
+		System.out.println("Soma dos números pares: " + somaPares);
 	}
 }
